bfaee2173ab13a4bf699307fa1d114ecc1930f50
RounokRabbi <rounok.cse.20220104096@aust.edu>
2024-12-21 00:22:44 +0600

    updated router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import { Outlet, Route, Routes } from 'react-router';
+import { Outlet, Route, Routes } from 'react-router-dom';
 import BaseLayout from './views/BaseLayout';
 import Home from './views/Home';
 import AvailableCats from './views/AvailableCats';
+import ContactUs from './views/ContactUs';
+import AboutUs from './views/AboutUs';
+import Donate from './views/Donate';
 
 function App() {
   return (
@@ -13,8 +16,12 @@ function App() {
           </BaseLayout>
         }
       >
-        <Route path={'/'} element={<Home />} />
-        <Route path={'/available-cats'} element={<AvailableCats />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/available-cats" element={<AvailableCats />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/donate" element={<Donate />} />
+        
       </Route>
     </Routes>
   );
